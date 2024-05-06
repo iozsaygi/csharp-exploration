afa67ed14e215e891c916815cf8bb51b6fdf0115
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-06 20:28:49 +0300

    Added exploration for 'PrimaryConstructors'

---
+++ Source/PrimaryConstructors/PrimaryConstructorsExploration.cs
@@ -0,0 +1,37 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-12#primary-constructors
+
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Core.Source.PrimaryConstructors;
+
+public interface IEntityShape
+{
+    byte Calculate();
+}
+
+// The constructor declaration carried to the class definition line.
+// It can still implement interface and support inheritance.
+public class Entity(byte index, string name) : IEntityShape
+{
+    public readonly byte Index = index;
+    public readonly string Name = name;
+
+    // Interface implementation.
+    public byte Calculate()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Normal methods can still be declared inside class.
+    public string GetEntityId()
+    {
+        return string.Concat(Name, Index.ToString());
+    }
+}
+
+// Readonly structs are also available to usage with primary constructors.
+public readonly struct EntityEntry(int volume, string id)
+{
+    public readonly int Volume = volume;
+    public readonly string Id = id;
+}
