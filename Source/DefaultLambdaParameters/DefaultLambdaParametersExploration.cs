df07749b7c647089b5c3e806d9485baa770861d6
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-06 21:06:05 +0300

    Added exploration for 'DefaultLambdaParameters'

---
+++ Source/DefaultLambdaParameters/DefaultLambdaParametersExploration.cs
@@ -0,0 +1,8 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-12#default-lambda-parameters
+
+// We can now define default value for the lambda parameter.
+// ReSharper disable once ConvertToLocalFunction
+
+var lambda = (byte count = 5) => $"The passed count is {count}";
+
+lambda();
